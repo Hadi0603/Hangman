458d7cc90a0ce25b4a00c2967717ade13e938509
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2025-05-14 16:56:22 +0500

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,22 +7,56 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    //[SerializeField] private float gameTime = 60f;
-    //private bool isBlinking = false;
-    //private bool isPaused = false;
-    //[SerializeField] private Text timerText;
-    [FormerlySerializedAs("gameOverUI")] [SerializeField] private GameObject levelWonUI;
-    [SerializeField] private GameObject levelLostUI;
-    //[SerializeField] GameObject pauseUI;
+    [FormerlySerializedAs("gameOverUI")] [SerializeField] private CanvasGroup levelWonUI;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private CanvasGroup levelLostUI;
+    [SerializeField] private GameObject lostPanel;
+    [SerializeField] private CanvasGroup pauseUI;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject pauseBtn;
+    [SerializeField] private Text timerText;
+    [SerializeField] private float gameTime = 30f;
+    private bool isBlinking = false;
+    private bool isPaused = false;
     [SerializeField] QuizController quizController;
 
     private void Awake()
     {
-        Time.timeScale = 1f;
-        //StartCoroutine(TimerCountdown());
+        levelWonUI.alpha = 0f;
+        winPanel.transform.localPosition = new Vector2(0, +Screen.height);
+        levelLostUI.alpha = 0f;
+        lostPanel.transform.localPosition = new Vector2(0, +Screen.height);
+        pauseUI.alpha = 0f;
+        pausePanel.transform.localPosition = new Vector2(0, +Screen.height);
+        timerText.color = Color.white;
+        StartCoroutine(TimerCountdown());
     }
 
-    /*private IEnumerator TimerCountdown()
+    
+    public void TriggerGameWon()
+    {
+        StopCoroutine(TimerCountdown());
+        isPaused = true;
+        levelWonUI.gameObject.SetActive(true);
+        levelWonUI.LeanAlpha(1, 0.5f);
+        pauseBtn.SetActive(false);
+        winPanel.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+        if (GameController.levelToLoad < 15)
+        {
+            PlayerPrefs.SetInt("levelToLoad", ++GameController.levelToLoad);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void GameOver()
+    {
+        levelLostUI.gameObject.SetActive(true);
+        pauseBtn.SetActive(false);
+        timerText.enabled = false;
+        levelLostUI.LeanAlpha(1, 0.5f);
+        lostPanel.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+    }
+    private IEnumerator TimerCountdown()
     {
         while (gameTime > 0)
         {
@@ -63,31 +97,26 @@ public class UIManager : MonoBehaviour
         timerText.color = originalColor;
         isBlinking = false;
     }
-
     public void OpenPauseMenu()
     {
-        pauseUI.SetActive(true);
-        Time.timeScale = 0f;
+        pauseUI.gameObject.SetActive(true);
+        pauseUI.LeanAlpha(1, 0.5f);
+        pauseBtn.SetActive(false);
+        pausePanel.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+        isPaused = true;
     }
 
     public void ClosePauseMenu()
     {
-        pauseUI.SetActive(false);
-        Time.timeScale = 1f;
-    }*/
-    public void TriggerGameWon()
-    {
-        levelWonUI.SetActive(true);
-        if (GameController.levelToLoad < 15)
-        {
-            PlayerPrefs.SetInt("levelToLoad", ++GameController.levelToLoad);
-        }
-        PlayerPrefs.Save();
+        pauseUI.LeanAlpha(0, 0.5f);
+        pausePanel.LeanMoveLocalY(+Screen.height, 0.5f).setEaseInExpo();
+        pauseBtn.SetActive(true);
+        isPaused = false;
+        Invoke(nameof(DisablePauseUI), 0.5f);
     }
 
-    public void GameOver()
+    private void DisablePauseUI()
     {
-        levelLostUI.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
     }
-
 }
